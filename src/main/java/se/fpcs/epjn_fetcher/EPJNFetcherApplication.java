46165eaf1138cdcd95088742a1465fbc420a2728
Tomas Faal Petersson <tomas.faal.petersson@irori.se>
2025-05-08 15:01:09 +0200

    basic spring boot setup

---
+++ src/main/java/se/fpcs/epjn_fetcher/EPJNFetcherApplication.java
@@ -0,0 +1,13 @@
+package se.fpcs.epjn_fetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EPJNFetcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EPJNFetcherApplication.class, args);
+    }
+
+}
